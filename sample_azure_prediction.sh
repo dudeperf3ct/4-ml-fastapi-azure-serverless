4d1980c73ce89a000b5b024ea3f5f65cf36b1a74
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2021-12-09 15:57:41 +0530

    add azure website curl request

---
+++ sample_azure_prediction.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+curl -X 'POST' \
+  'https://wine-ratings.azurewebsites.net/predict' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "fixed_acidity": 10.4,
+
+  "volatile_acidity": 0.26,
+  
+  "citric_acid": 0.48,
+  
+  "residual_sugar": 1.9,
+  
+  "chlorides": 0.066,
+  
+  "free_sulfur_dioxide": 6.0,
+  
+  "total_sulfur_dioxide": 10.0,
+  
+  "density": 0.99724,
+  
+  "ph": 3.33,
+  
+  "sulphates": 0.87,
+  
+  "alcohol": 10.9
+}'
